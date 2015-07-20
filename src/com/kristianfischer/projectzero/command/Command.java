f75bb7c716d042c5d14fc99805107a0c683849a7
SirFishy <khfishy@gmail.com>
2015-07-20 15:26:40 -0400

    Adding changes to command file.

--- src/com/kristianfischer/projectzero/command/Command.java
+++ src/com/kristianfischer/projectzero/command/Command.java
@@ -1,12 +1,23 @@
 package com.kristianfischer.projectzero.command;
 
 import com.kristianfischer.projectzero.gameobject.GameObject;
+import com.kristianfischer.projectzero.handler.GameHandler;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by kristianhfischer on 7/10/15.
  */
 public abstract class Command {
-    public Command() {}
+    protected List<GameObject> objectList;
+    public Command() {
+        objectList = new LinkedList<GameObject>();
+    }
+
+    public List<GameObject> getSpawnedObjects() {
+        return objectList;
+    }
 
     public abstract void execute(GameObject object);
     public abstract void stop(GameObject object);
